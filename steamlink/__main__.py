8d9bcfa20a201411d5e592839772aaa6d7f3f2c4
Andreas Wrede <andreas@wrede.ca>
2017-12-26 00:21:21 -0500

    provide default config values in structure

--- steamlink/__main__.py
+++ steamlink/__main__.py
@@ -3,6 +3,7 @@
 # Main program for a Stealink network
 
 import sys
+import os
 import traceback
 import syslog
 
@@ -14,7 +15,10 @@ import logging
 logger = logging.getLogger()
 
 # SteamLink project imports
-from steamlink.mqtt import Mqtt
+from steamlink.mqtt import (
+	Mqtt,
+	Mqtt_Broker
+)
 
 from steamlink.steamlink import (
 	Room,
@@ -47,7 +51,7 @@ from steamlink.testdata import TestData
 from steamlink.const import (
 	LIB_DIR,
 	PROJECT_PACKAGE_NAME, 
-    INDEX_HTML, 
+	INDEX_HTML, 
 	__version__
 )
 
@@ -58,6 +62,64 @@ class GracefulExit(SystemExit):
 
 def raise_graceful_exit():
 	raise GracefulExit()
+
+DEFAULT_CONF = {
+ 	'general': {
+		'mqtt_broker': 'mqtt_broker',
+		'ping_timeout': 30,
+	},
+	'Steam': {
+		'id': 0,
+		'name': 'sample1',
+		'namespace': '/sl',
+		'description': 'SteamLink Sample',
+	},
+	'tests': {
+	},
+	'mqtt': {
+#		'clientid': 'CLIENTID',
+#		'username': 'USERNAME',
+#		'password': 'PASSWORD',
+		'server': 'localhost',
+		'port': 1883,
+#		'ssl_certificate': 'ca.crt',
+
+		'prefix': 'SteamLink',
+		'data': 'data',
+		'control': 'control',
+	},
+
+	'console': {
+		'host': '0.0.0.0',
+		'port': 5050,
+		'shutdown_timeout': 10,        # seconds to wait for web server shutdown
+		'namespace': '/sl',
+		'prefix': 'SteamLinkWeb',
+		'index': 'index.htm',         # root page 
+	},
+	'mqtt_broker':  {
+		'listeners': {
+			'default': {
+				'type': 'tcp',
+				'bind': '0.0.0.0:1883',
+			},
+			'ws-mqtt': {
+				'bind': '127.0.0.1:8080',
+				'type': 'ws',
+				'max_connections': 10,
+			},
+		},
+		'sys_interval': 10,
+		'auth': {
+			'allow-anonymous': True,
+#			'password-file': os.path.join(os.path.dirname(os.path.realpath(__file__)), 'passwd'),
+			'plugins': [
+#				'auth_file', 
+				'auth_anonymous',
+			]
+		}
+	}
+}
 
 
 
@@ -102,7 +164,10 @@ def steamlink_main() -> int:
 		return(rc)
 	
 	# load config 
-	conf = loadconfig(conff)
+	if conff is None:
+		conf = DEFAULT_CONF
+	else:
+		conf = loadconfig(DEFAULT_CONF, conff)
 
 	# Daemon functions
 	if cl_args.pid_file:
@@ -130,7 +195,7 @@ def steamlink_main() -> int:
 	conf_general = conf.get('general',{})
 	conf_console = conf.get('console',{})
 	conf_steam = conf.get('Steam',{})
-
+	conf_broker = conf.get('mqtt_broker',{})
 	conf_mqtt = conf.get('mqtt',{})
 	
 	namespace = conf_general.get('namespace','/sl')
@@ -138,10 +203,15 @@ def steamlink_main() -> int:
 	#sl_log = LogData(conf['logdata'])
 	sl_log = None
 
+	if not conf_broker is None: 
+		logger.debug("startup: create MQTT Broker")
+		mqtt_broker = Mqtt_Broker(conf_broker, loop=aioloop)
+		logger.debug("startup: start MQTT Broker")
+		aioloop.run_until_complete(mqtt_broker.start())
+
 	logger.debug("startup: create Mqtt")
 	mqtt = Mqtt(conf_mqtt, sl_log)
 
-	logger.debug("startup: starting Mqtt")
 	aioloop.run_until_complete(mqtt.start())
 	
 	ping_timeout = conf_general.get('ping_timeout','10')
@@ -156,25 +226,31 @@ def steamlink_main() -> int:
 
 	logger.debug("startup: create WebApp")
 	webapp = WebApp(namespace, sio, conf_console, loop=aioloop)
+	logger.debug("startup: start webapp")
+	aioloop.run_until_complete(webapp.start())
 
 	logger.debug("startup: create Steam")
 	steam = Steam(conf_steam, mqtt, sio)
 	steam.attach(webapp, namespace)
 
-	logger.debug("startup: starting coros")
-	coros = []
-	coros.append(steam.start())
-	coros.append(webapp.start())
-	coros.append(webapp.qstart())
+	logger.debug("startup: start steam")
+	aioloop.run_until_complete(steam.start())
 
+	coros = []
 	if cl_args.testdata:
+		testconfigs = conf['tests']
 		logger.debug("startup: create TestData")
-		TestTask = TestData(conf['testdata'], aioloop)
+		if not cl_args.testdata in testconfigs:
+			logger.error("testdata: no section '%s' in config file", cl_args.testdata)
+			sys.exit(1)
+		TestTask = TestData(testconfigs[cl_args.testdata], aioloop)
 		logger.debug("startup: starting TestData")
 		coros.append(TestTask.start())
 	else:
 		TestTask = None
 
+	coros.append(webapp.qstart())
+	logger.debug("startup: starting coros")
 	try:
 		aioloop.run_until_complete(asyncio.gather(
 			*coros
@@ -182,6 +258,8 @@ def steamlink_main() -> int:
 		aioloop.run_forever()
 	except (GracefulExit, KeyboardInterrupt):
 		logger.debug("terminating")
+	except hbmqtt.errors.NoDataException as e:
+		logger.notice("coros run_until: hbmqtt.errors.NoDataException: %s", e)
 	
 	
 	# Shutdown
@@ -191,6 +269,8 @@ def steamlink_main() -> int:
 	
 	aioloop.run_until_complete(mqtt.stop())
 	aioloop.run_until_complete(steam.stop())
+	if not conf_broker is None: 
+		aioloop.run_until_complete(mqtt_broker.stop())
 	
 	logger.info("done")
 
@@ -205,8 +285,7 @@ def main() -> int:
 	except:
 		rc = 127
 		exc_type, exc_value, exc_traceback = sys.exc_info()
-		tb = traceback.format_exception(exc_type, exc_value,
-                                          exc_traceback)
+		tb = traceback.format_exception(exc_type, exc_value, exc_traceback)
 		if not daemon:
 			for l in tb:
 				print(l.rstrip('\n'))
