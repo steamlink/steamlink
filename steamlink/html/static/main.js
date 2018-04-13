e6553f2738fd533d9380a151d96e2366a41423a9
Udit Vira <udit.vira@gmail.com>
2018-04-13 16:19:49 -0400

    more self

--- steamlink/html/static/main.js
+++ steamlink/html/static/main.js
@@ -10,7 +10,8 @@ function Stream(sock, config, on_new_message) {
   this.start_key = {};
   this.end_key = {};
   this.cache = [];
-
+  var self = this;
+  
   this.startStream = function() {
     console.log("Starting stream with config:");
     console.log(this.config);
@@ -23,14 +24,15 @@ function Stream(sock, config, on_new_message) {
       return_children: this.config.return_children,
       stream_tag: this.config.stream_tag,
       force: this.config.force
-    }, function(data){ // on ack
+    }, function (data){ // on ack
       if (data.error) {
         console.log("Err: " + data.error);
       } else { // store key field and record type
         console.log("Ack rcvd");
         console.log(data);
-        this.record_type = data.record_type;
-        this.key_field = data.key_field;
+        self.record_type = data.record_type;
+        self.key_field = data.key_field;
+        console.log(self);
       }
     });
   };
@@ -40,36 +42,36 @@ function Stream(sock, config, on_new_message) {
     console.log(data);
     // back-end can ask for either an add, modify, or delete
     // first see if we have record in cache
-    console.log(this);
+    console.log();
     console.log("Cache is:");
-    console.log(this.cache);
-    var foundIndex = this.cache.findIndex(function(e){
-      return e[this.key_field] === data[this.key_field];
+    console.log(self.cache);
+    var foundIndex = self.cache.findIndex(function(e){
+      return e[self.key_field] === data[self.key_field];
     });
     if (_del_key in data) { // if delete:
       if (foundIndex >= 0) { // if key exists in cache
-        this.cache.splice(foundIndex, 1); 
+        self.cache.splice(foundIndex, 1); 
       } else { // key doesn't exist in cache:
         console.log("debug: ignoring _del_key: " + data._del_key);
       }
     } else { // if add/modify:
       if (foundIndex >= 0) { // if key exists in cache
         // update the cached record
-        this.cache[foundIndex] = data; 
+        self.cache[foundIndex] = data; 
       } else { // if key not in cache
         // find insertion point
-        insertionIndex = this.cache.findIndex(function(e) {
+        insertionIndex = self.cache.findIndex(function(e) {
           // assume cache is ordered by key_field
-          return (e[this.key_field] > data[this.key_field])
+          return (e[self.key_field] > data[self.key_field])
           });
         if (insertionIndex >= 0) { // if insertion index is found
-          this.cache.splice(insertionIndex, 0, data);
-          if (data[this.key_field] < this.start_key) {
-            this.start_key = data[this.key_field];
+          self.cache.splice(insertionIndex, 0, data);
+          if (data[self.key_field] < self.start_key) {
+            self.start_key = data[self.key_field];
           }
         } else { // must insert at end
-          this.cache.push(data);
-          this.end_key = data[this.key_field];
+          self.cache.push(data);
+          self.end_key = data[self.key_field];
         }
         // TODO: cache pruning?
       }  
@@ -77,8 +79,7 @@ function Stream(sock, config, on_new_message) {
     on_new_message(data);
   };
 
-  var self = this;
-  sock.on(this.config.stream_tag, self.newStreamData);
+  sock.on(self.config.stream_tag, self.newStreamData);
 }
 
 // On Document Ready
