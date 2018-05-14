270bdb0b339063925aa864e18a8f305c37b781a7
Andreas Wrede <andreas@wrede.ca>
2018-05-14 12:28:44 -0400

    first working version of python based node

--- steamlink/pynode.py
+++ steamlink/pynode.py
@@ -8,28 +8,31 @@ import sys
 
 import logging
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 from .mqtt import Mqtt
 from .steamlink import (
 	SL_OP,
 	SL_NodeCfgStruct,
 	BasePacket,
+	WaitForAck, 
+	SL_MAX_MESSAGE_LEN, 
+	SL_ACK_WAIT,
+	MAX_RESEND_COUNT,
 )
 from .util import phex
 
 
 
 class PyNode:
-	def __init__(self, nodecfg, conf_mqtt):
+	def __init__(self, nodecfg, conf_mqtt, loop):
 		self.nodecfg = nodecfg
 		self.slid = self.nodecfg.slid
 		self.conf_mqtt = conf_mqtt
 		self.mqtt = None
-		self.loop = asyncio.get_event_loop()
+		self.loop = loop
 		logger.info("Start")
-		self.on_receive = None
-
+		self.receive_q = None
+		self.via = []
 		self.packets_sent = 0
 		self.packets_sent = 0
 		self.packets_resent = 0
@@ -37,27 +40,30 @@ class PyNode:
 		self.packets_missed = 0
 		self.packets_duplicate = 0
 		self.pkt_num = 0
+		self.wait_for_AN = WaitForAck(SL_ACK_WAIT)
+		self.wait_handle = None
 
 		self.last_packet_tx_ts = 0
 
+		self.pstate = "initial"
 
 
-	def start(self):
+	async def start(self):
 		if self.mqtt is not None:
 			logger.error("start called more than once")
 			sys.exit(1)
-		self.mqtt = Mqtt(self.conf_mqtt)
+		self.mqtt = Mqtt(self.conf_mqtt, as_node=True)
 		logger.debug("startup: create Mqtt")
-		self.loop.run_until_complete(self.mqtt.start())
+		self.mqtt.set_msg_callback(self.on_control_msg)
+		await self.mqtt.start()
+		self.send_online_to_store()
+		logger.debug("startup: done")
 
-		self.mqtt.set_msg_callback(self.on_data_msg)
 
-		self.signon()
-
-	def on_data_msg(self, client, userdata, msg):
-		logger.info("on_data_msg: got %s", msg)
+	def on_control_msg(self, client, userdata, msg):
+		if logging.DBG > 0: logger.info("on_control_msg: got %s", msg.payload)
 		try:
-			pkt = BasePacket(pkt=msg.payload)
+			pkt = BasePacket(slnode=self, pkt=msg.payload)
 		except SteamLinkError as e:
 			logger.warning("mqtt: pkt dropped: '%s', steamlink error %s", msg.payload, e)
 			return
@@ -66,70 +72,121 @@ class PyNode:
 			return
 
 		if pkt.slid != self.slid:
-			logger.warning("mqtt: pkt dropped: '%s', not for us", msg.payload)
+			if logging.DBG > 1: logger.warning("mqtt: pkt '%s', not for us", msg.payload)
 			return
 
 
-	def signon(self):
-		pkt = BasePacket(self.slid,  SL_OP.val('ON'), payload=self.nodecfg)
-		pkt.pkt_num = self.get_pkt_num()
-		logger.info("send: %s", pkt)
+		if pkt.sl_op == SL_OP.AN:		# Ack
+			if self.wait_handle is not None:
+				self.wait_handle.cancel()
+				self.wait_handle = None
+			if self.wait_for_AN is not None:
+				pkt = self.wait_for_AN.stop_wait()
+		elif pkt.sl_op == SL_OP.SC:	
+			rc = self.handle_sc(pkt.payload)
+			
+		elif pkt.sl_op == SL_OP.DN:	
+			asyncio.ensure_future(self.receive_q.put(pkt.payload), loop=self.loop)
+		elif pkt.sl_op == SL_OP.SS:	
+			self.handle_sc(pkt.payload)
+		else:
+			logger.error("mqtt: for now, not handling %s", pkt)
 
-	def get_pkt_num(self):
+
+	def process_ack_timeout(self):
+		logger.info("process_ack_timeout")
+		self.wait_handle = None
+		if self.wait_for_AN.inc_resend_count() > MAX_RESEND_COUNT:
+			logger.info("resend limit reached for %s, giving up", self.wait_for_AN)
+			self.wait_for_AN.clear_wait()
+		else:
+			pkt = self.wait_for_AN.restart_wait()
+			self.publish_pkt(pkt, resend=True)
+	
+
+	def handle_sc(self, payload):
+		if logging.DBG > 1: logger.debug("handle sc: %s", payload)
+		# XXX: actually config  stuff
+		self.send_ack_to_store(0)
+
+
+	def send_data_to_store(self, data):
+		sl_pkt = BasePacket(self, sl_op=SL_OP.DS, payload=data)
+		self.publish_pkt(sl_pkt)
+		self.wait_for_AN.set_wait(sl_pkt)
+
+
+	def send_ack_to_store(self, code):
+		sl_pkt = BasePacket(slnode=self, sl_op=SL_OP.AS, payload=chr(code))
+		self.publish_pkt(sl_pkt)
+
+
+	def send_online_to_store(self):
+		sl_pkt = BasePacket(self,  SL_OP.ON, payload=self.nodecfg)
+		logger.info("send: %s", sl_pkt)
+		if self.publish_pkt(sl_pkt):
+			self.wait_for_AN.set_wait(sl_pkt)
+			self.wait_handle = self.loop.call_later(SL_ACK_WAIT, self.process_ack_timeout)
+			return True
+		else:
+			return False	
+
+
+	def set_pkt_number(self, pkt):
 		self.pkt_num += 1
 		if self.pkt_num == 0:
 			self.pkt_num = 1
 		return self.pkt_num
 
 
-	def publish_pkt(self, sl_pkt=None, resend=False, sub="control"):
+	def publish_pkt(self, sl_pkt=None, resend=False, sub="data"):
 		if resend:
-			logger.debug("resending pkt: %s", sl_pkt)
+			if logging.DBG > 1: logger.debug("resending pkt: %s", sl_pkt)
 			self.packets_resent += 1
 		else:
-			if self.is_waiting_for_AS() and sl_pkt.sl_op != SL_OP.AN:
-				logger.error("attempt to send pkt while waiting for AS, ignored: %s", sl_pkt)
+			if self.wait_for_AN.is_waiting() and sl_pkt.sl_op != SL_OP.AN:
+				logger.error("attempt to send pkt while waiting for AN, ignored: %s", sl_pkt)
 				self.packets_dropped += 1
-				return
+				return False
 		if len(sl_pkt.pkt) > SL_MAX_MESSAGE_LEN:
 			logger.error("publish pkt to long(%s): %s", len(sl_pkt.pkt), sl_pkt)
-			return
+			return False
 		self.packets_sent += 1
 		if logging.DBG > 1: logger.debug("publish_pkt %s", sl_pkt )
 		self.mqtt.publish(self.slid, sl_pkt.pkt, sub=sub)
 		self.last_packet_tx_ts = time.time()
+		return True
 
 
 	def send(self, packet, slid=1):
 		logger.info("sending: '%s' to '%s'", packet, slid)
-		sl_op = SL_OP.val('DS')
-		payload=packet
-		pkt = BasePacket(self.slid, sl_op, 0, payload)
-		pkt.pkt_num = self.get_pkt_num()
-
-		logger.info("send: %s", pkt)
-		self.publish_pkt(pkt)
+		self.send_data_to_store(packet)
 
 
-	def register_receive_handler(self, on_receive):
-		self.on_receive = on_receive
+
+	def register_receive_queue(self, receive_q):
+		self.receive_q = receive_q
 
 
 def setup_logging(loglvl):
-	logging.DBG = -1
-	ch = logging.StreamHandler()
-	ch.setLevel(loglvl)
-#	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-	formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-	ch.setFormatter(formatter)
-	logger.addHandler(ch)
+	FORMAT='%(name)s - %(levelname)s - %(message)s'
+	logging.basicConfig(level=loglvl, format=FORMAT)
+
+	logging.DBG = 0
+	logging.DBGK = []
+#	ch = logging.StreamHandler()
+##	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#	formatter = logging.Formatter(FORMAT)
+#	ch.setFormatter(formatter)
+#	ch.setLevel(loglvl)
+#	logger.addHandler(ch)
 
 
 if __name__ == '__main__':
 
 	mqtt_conf = {
 		'clientid': 'pynode_%s' % time.time(),
-		'username': 'demo1',
+		'username': 'demonode0',
 		'password': 'ui712lkm921d',
 		'server': 'mqtt.steamlink.net',
 		'port': 1883,
@@ -138,15 +195,43 @@ if __name__ == '__main__':
 		'control': 'control',
 	}
 
+
+	async def process_incoming(receive_q):
+		while True:
+			pkt = await receive_q.get()
+			print("Got one", pkt)
+		receive_q.task_done()
+		
+
+	async def produce_outgoing(sl_node):
+		await sl_node.start()
+		while True:
+			await asyncio.sleep(10)
+			sl_node.send("Hello PyWorld")
+
+
+	async def run(loop):
+		logger.info("PyNode Test")
+		nodecfg = SL_NodeCfgStruct(slid=401, name="PyNode401", description="Test Py")
+		
+		# create a Queue to receive incoming messages and start the re
+		receive_q = asyncio.Queue()
+		consumer = asyncio.ensure_future(process_incoming(receive_q))
+
+		# create our Node and register the receive queue with it
+		sl_node = PyNode(nodecfg, mqtt_conf, loop)
+		sl_node.register_receive_queue(receive_q)
+
+		# start outgoing 
+		await produce_outgoing(sl_node)
+
+		# exit when the queue closes
+		await receive_q.join()
+
+			
 	setup_logging(logging.DEBUG)
 
-	logger.info("PyNode Test")
-	nodecfg = SL_NodeCfgStruct(slid=401, name="PyNode401", description="Test Py")
-
-	test = PyNode(nodecfg, mqtt_conf)
-	test.start()
-	
-	test.send("Hello World")
-
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(run(loop))
 	logger.info("done")
 
